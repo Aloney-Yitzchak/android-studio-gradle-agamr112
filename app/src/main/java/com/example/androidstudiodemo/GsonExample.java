837ff3f41d915be2b670fd06d6699c0d5ef0c39e
agamr112 <agam.r@a-y.org.il>
2026-02-15 16:00:39 +0200

    Update GsonExample.java

--- app/src/main/java/com/example/androidstudiodemo/GsonExample.java
+++ app/src/main/java/com/example/androidstudiodemo/GsonExample.java
@@ -1,17 +1,9 @@
 package com.example.androidstudiodemo;
 
-// TODO: הוסף import לGson כאן
+import com.google.gson.Gson; // TODO: הוסף import לGson כאן
 
-
-/**
- * דוגמה פשוטה לשימוש בספריית Gson
- * Gson עוזר לנו להמיר אובייקטים ל-JSON ולהיפך
- */
 public class GsonExample {
     
-    /**
-     * מחלקה פשוטה שמייצגת משתמש
-     */
     static class User {
         String name;
         int age;
@@ -25,20 +17,17 @@ public class GsonExample {
     }
     
     public static void main(String[] args) {
-        // TODO: צור אובייקט Gson
-        // Gson gson = new Gson();
+        Gson gson = new Gson();
         
-        // יצירת אובייקט User
         User user = new User("דני", 17, "תל אביב");
         
-        // TODO: המר את האובייקט ל-JSON והדפס אותו
-        // String json = gson.toJson(user);
-        // System.out.println("JSON: " + json);
+        String json = gson.toJson(user);
+        System.out.println("JSON: " + json);
         
-        
-        // TODO (בונוס): נסה להמיר JSON חזרה לאובייקט
-        // String jsonString = "{\"name\":\"שרה\",\"age\":16,\"city\":\"ירושלים\"}";
-        // User newUser = gson.fromJson(jsonString, User.class);
-        // System.out.println("Name: " + newUser.name);
+        String jsonString = "{\"name\":\"שרה\",\"age\":16,\"city\":\"ירושלים\"}";
+        User newUser = gson.fromJson(jsonString, User.class);
+        System.out.println("Name: " + newUser.name);
+        System.out.println("Age: " + newUser.age);
+        System.out.println("City: " + newUser.city);
     }
 }
